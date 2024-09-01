ef30bbc9c53f2def0ae1bd4d1094a76c6b160a41
Irakli Menteshashvili <ir.menteshashvili@gmail.com>
2024-09-01 18:05:19 +0400

    add slider

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+//burger menu
 let burger = document.querySelector(".burger");
 let xmark = document.querySelector(".xmark");
 let mobileMenu = document.querySelector(".nav_header");
@@ -14,3 +15,39 @@ xmark.addEventListener("click", () => {
   xmark.style.display = "none";
   burger.style.display = "block";
 });
+
+//slider
+const sliderImg = document.getElementsByClassName("slider");
+const right = document.getElementById("left");
+const left = document.getElementById("right");
+
+let activeIndex = 0;
+
+const unvisible = (index) => {
+  sliderImg[index].classList.remove("visible");
+};
+
+const visible = (index) => {
+  sliderImg[index].classList.add("visible");
+};
+
+right.addEventListener("click", () => {
+  unvisible(activeIndex);
+  if (activeIndex == sliderImg.length - 1) {
+    activeIndex = 0;
+  } else {
+    activeIndex += 1;
+  }
+  visible(activeIndex);
+});
+
+left.addEventListener("click", () => {
+  unvisible(activeIndex);
+  if (activeIndex == 0) {
+    activeIndex = sliderImg.length - 1;
+  } else {
+    activeIndex -= 1;
+  }
+  visible(activeIndex);
+});
+
